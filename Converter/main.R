50ceea3761328e0c6a9a35b5e63f9816397f10af
DanielBrMz <a01254805@tec.mx>
2023-05-25 00:00:00 -0600

    redering enhanced

--- Converter/main.R
+++ Converter/main.R
@@ -27,20 +27,29 @@ server <- function(input, output) {
     
     # Add states and transitions based on rules
     for (rule in rules) {
-      # Check if the rule is not empty
-      if (length(rule) > 1) {
+      # Check if the rule is not empty and has enough characters
+      if (length(rule) > 1 && nchar(rule[2]) >= 1) {
         antecedent <- rule[1]
-        consequent <- substr(rule[2], 2, 2)  # Extract the state from the consequent
         transition <- substr(rule[2], 1, 1)  # Extract the transition symbol from the consequent
         
         # Check if the rule is valid
-        if (nchar(transition) > 0 && nchar(consequent) > 0) {
+        if (nchar(transition) > 0) {
           # Add states if they don't exist yet
           if (!antecedent %in% V(g)$name) {
             g <- add.vertices(g, nv = 1, name = antecedent)
           }
-          if (!consequent %in% V(g)$name) {
-            g <- add.vertices(g, nv = 1, name = consequent)
+          
+          # Determine the consequent
+          if (nchar(rule[2]) > 1) {
+            consequent <- substr(rule[2], 2, 2)  # Extract the state from the consequent
+            if (!consequent %in% V(g)$name) {
+              g <- add.vertices(g, nv = 1, name = consequent)
+            }
+          } else {
+            consequent <- "Z"  # Set to 'Z' if no consequent state is specified
+            if (!consequent %in% V(g)$name) {
+              g <- add.vertices(g, nv = 1, name = consequent)
+            }
           }
           
           # Add transition
@@ -48,11 +57,6 @@ server <- function(input, output) {
           E(g, path=c(antecedent, consequent))$name <- transition
         }
       }
-    }
-    
-    # If there is a state with no outgoing edges, rename it to 'Z'
-    if (length(which(degree(g, mode="out") == 0)) > 0) {
-      V(g)[which(degree(g, mode="out") == 0)]$name <- "Z"
     }
     
     # Return the graph
@@ -63,7 +67,7 @@ server <- function(input, output) {
   output$automatonPlot <- renderPlot({
     g <- reactiveAutomaton()
     
-    if (nchar(input$grammar) > 0) {
+    if (nchar(input$grammar) > 0 && gorder(g) > 0) {
       E(g)$label <- E(g)$name  # Label the edges with the transition symbols
     }
     
